2d98aae0d5441582379b93b52429a2e3bdc1aaad
SomeForNull <1400866473@qq.com>
2024-03-11 00:01:39 +0800

    功能性优化

--- yunshu-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
+++ yunshu-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
@@ -100,5 +100,20 @@ public class MainGenerator {
         String jarName=String.format("%s-%s-jar-with-dependencies.jar",meta.getName(),meta.getVersion());
         String jarPath="target/"+jarName;
         ScriptGenerator.doGenerator(shellOutputFilePath,jarPath);
+
+        //构建精简版的程序（产物包）
+        String distOutputPath=outputPath+"-dist";
+        //拷贝jar包
+        String targetAbsolutePath=distOutputPath+File.separator+"target";
+        FileUtil.mkdir(targetAbsolutePath);
+        String jarAbsolutePath=outputPath+File.separator+jarPath;
+        FileUtil.copy(jarAbsolutePath,targetAbsolutePath,true);
+        //拷贝脚本文件
+        FileUtil.copy(shellOutputFilePath,distOutputPath,true);
+        FileUtil.copy(shellOutputFilePath+".bat",distOutputPath,true);
+        //拷贝源模板文件
+        FileUtil.copy(sourceCopyPath,distOutputPath,true);
+        //拷贝README.md
+        FileUtil.copy(outputPath+File.separator+"README.md",distOutputPath,true);
     }
 }
