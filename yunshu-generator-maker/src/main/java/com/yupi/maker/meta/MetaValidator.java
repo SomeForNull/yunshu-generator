4e36aaf5094a474c234cf8e39370626e0b2f8382
SomeForNull <1400866473@qq.com>
2024-03-12 00:32:25 +0800

    可扩展性优化

--- yunshu-generator-maker/src/main/java/com/yupi/maker/meta/MetaValidator.java
+++ yunshu-generator-maker/src/main/java/com/yupi/maker/meta/MetaValidator.java
@@ -4,6 +4,9 @@ import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import com.yupi.maker.meta.enums.FileGenerateTypeEnum;
+import com.yupi.maker.meta.enums.FileModelTypeEnum;
+import com.yupi.maker.meta.enums.FileTypeEnum;
 
 import java.io.File;
 import java.nio.file.Paths;
@@ -38,7 +41,7 @@ public class MetaValidator {
             }
             String modelInfoType = modelInfo.getType();
             if (StrUtil.isEmpty(modelInfoType)) {
-                modelInfo.setType("String");
+                modelInfo.setType(FileModelTypeEnum.STRING.getValue());
             }
         }
     }
@@ -63,8 +66,8 @@ public class MetaValidator {
         String outputRootPath = StrUtil.blankToDefault(fileConfig.getOutputRootPath(), defaultOutputRootPath);
         fileConfig.setOutputRootPath(outputRootPath);
 
-        String defaultType = "dir";
-        String fileConfigType = StrUtil.blankToDefault(fileConfig.getType(), defaultType);
+
+        String fileConfigType = StrUtil.blankToDefault(fileConfig.getType(), FileTypeEnum.DIR.getValue());
         fileConfig.setType(fileConfigType);
 
         // fileInfo 默认值
@@ -88,9 +91,9 @@ public class MetaValidator {
             if (StrUtil.isBlank(type)) {
                 // 无文件后缀
                 if (StrUtil.isBlank(FileUtil.getSuffix(inputPath))) {
-                    fileInfo.setType("dir");
+                    fileInfo.setType(FileTypeEnum.DIR.getValue());
                 } else {
-                    fileInfo.setType("file");
+                    fileInfo.setType(FileTypeEnum.File.getValue());
                 }
             }
             // generateType：如果文件结尾不为 Ftl，generateType 默认为 static，否则为 dynamic
@@ -98,9 +101,9 @@ public class MetaValidator {
             if (StrUtil.isBlank(generateType)) {
                 // 为动态模板
                 if (inputPath.endsWith(".ftl")) {
-                    fileInfo.setGenerateType("dynamic");
+                    fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
                 } else {
-                    fileInfo.setGenerateType("static");
+                    fileInfo.setGenerateType(FileGenerateTypeEnum.STATIC.getValue());
                 }
             }
         }
