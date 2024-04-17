ef129939288f559e22879fc9d85f8462f9fb62e1
SomeForNull <1400866473@qq.com>
2024-04-17 08:45:32 +0800

    移除无用引入

--- yunshu-generator-maker/src/main/java/com/yupi/maker/generator/ScriptGenerator.java
+++ yunshu-generator-maker/src/main/java/com/yupi/maker/generator/ScriptGenerator.java
@@ -1,6 +1,5 @@
 package com.yupi.maker.generator;
 import cn.hutool.core.io.FileUtil;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
