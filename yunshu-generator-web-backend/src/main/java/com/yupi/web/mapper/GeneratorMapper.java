4617e54cb3cd9eaf1b7f3cc314a66a75917e6c12
SomeForNull <1400866473@qq.com>
2024-07-28 09:57:32 +0800

    后端初始化

---
+++ yunshu-generator-web-backend/src/main/java/com/yupi/web/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.web.model.entity.Generator;
+
+/**
+ * @author 14008
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2024-07-24 20:24:03
+ * @Entity com.yupi.web.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
