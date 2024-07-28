d0ccd5496cb1180eebd9480501f2dc028d3c087a
SomeForNull <1400866473@qq.com>
2024-07-28 12:54:06 +0800

    COS 通用文件上传下载

--- yunshu-generator-web-backend/src/main/java/com/yupi/web/manager/CosManager.java
+++ yunshu-generator-web-backend/src/main/java/com/yupi/web/manager/CosManager.java
@@ -1,6 +1,8 @@
 package com.yupi.web.manager;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.yupi.web.config.CosClientConfig;
@@ -48,5 +50,16 @@ public class CosManager {
         PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key,
                 file);
         return cosClient.putObject(putObjectRequest);
+    }
+
+    /**
+     * 下载对象
+     *
+     * @param Key 对象唯一键
+     * @return
+     */
+    public COSObject getObject(String Key){
+        GetObjectRequest getObjectRequest = new GetObjectRequest(cosClientConfig.getBucket(), Key);
+        return cosClient.getObject(getObjectRequest);
     }
 }
